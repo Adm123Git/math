f1f292dae79d41a66b743ec1782bfbf5939316dc
Dmitry_Ushakov <Dmitry_Ushakov@russianpost.ru>
2021-12-27 15:07:58 +0300

    refact

--- src/ru/adm123/math/Main.java
+++ src/ru/adm123/math/Main.java
@@ -1,7 +1,7 @@
 package ru.adm123.math;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,48 +12,60 @@ import java.util.List;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        String str = "3 * 2 / 6.7 + 4 - 1 / 5";
-        System.out.println(str + " = " + calculate(str));
-    }
+	public static void main(String[] args) {
+		String str = "3 * 2 / 6.7 + 4 - 1 / 5";
+		System.out.println(str + " = " + calculate(str));
+	}
 
-    public static float calculate(@NotNull String str) {
-        List<String> digits = new ArrayList<>(Arrays.asList(str.split("\\s*\\*|/|\\+|-\\s*")));
-        List<String> signs = new ArrayList<>(Arrays.asList(str.split("\\s*\\d+(\\.\\d+)?\\s*")));
-        for (int i = digits.size() - 1; i > 0; i--) {
-            if (signs.get(i).equals("*") || signs.get(i).equals("/")) {
-                digits.set(i - 1, calcPair(digits, signs.get(i), i));
-                digits.remove(i);
-                signs.remove(i);
-            }
-        }
-        for (int i = digits.size() - 1; i > 0; i--) {
-            if (signs.get(i).equals("+") || signs.get(i).equals("-")) {
-                digits.set(i - 1, calcPair(digits, signs.get(i), i));
-                digits.remove(i);
-                signs.remove(i);
-            }
-        }
-        return Float.parseFloat(digits.get(0));
-    }
+	public static float calculate(@NotNull String str) {
+		List<String> digits = new ArrayList<>(Arrays.asList(str.split("\\s*\\*|/|\\+|-\\s*")));
+		List<String> signs = new ArrayList<>(Arrays.asList(str.split("\\s*\\d+(\\.\\d+)?\\s*")));
+		for (int i = digits.size() - 1; i > 0; i--) {
+			if (isDivisionOrMultiplication(signs.get(i))) {
+				calcProcess(digits, signs, i);
+			}
+		}
+		for (int i = digits.size() - 1; i > 0; i--) {
+			if (isComplexityOrSubtraction(signs.get(i))) {
+				calcProcess(digits, signs, i);
+			}
+		}
+		return Float.parseFloat(digits.get(0));
+	}
 
-    @Nullable
-    private static String calcPair(@NotNull List<String> digits,
-                                   @NotNull String sign,
-                                   int i) {
-        if (sign.equals("*")) {
-            return String.valueOf(Float.parseFloat(digits.get(i - 1)) * Float.parseFloat(digits.get(i)));
-        }
-        if (sign.equals("/")) {
-            return String.valueOf(Float.parseFloat(digits.get(i - 1)) / Float.parseFloat(digits.get(i)));
-        }
-        if (sign.equals("+")) {
-            return String.valueOf(Float.parseFloat(digits.get(i - 1)) + Float.parseFloat(digits.get(i)));
-        }
-        if (sign.equals("-")) {
-            return String.valueOf(Float.parseFloat(digits.get(i - 1)) - Float.parseFloat(digits.get(i)));
-        }
-        return null;
-    }
+	@Nullable
+	private static String calcPair(@NotNull List<String> digits,
+								   @NotNull String sign,
+								   int i) {
+		if (sign.equals("*")) {
+			return String.valueOf(Float.parseFloat(digits.get(i - 1)) * Float.parseFloat(digits.get(i)));
+		}
+		if (sign.equals("/")) {
+			return String.valueOf(Float.parseFloat(digits.get(i - 1)) / Float.parseFloat(digits.get(i)));
+		}
+		if (sign.equals("+")) {
+			return String.valueOf(Float.parseFloat(digits.get(i - 1)) + Float.parseFloat(digits.get(i)));
+		}
+		if (sign.equals("-")) {
+			return String.valueOf(Float.parseFloat(digits.get(i - 1)) - Float.parseFloat(digits.get(i)));
+		}
+		return null;
+	}
+
+	private static void calcProcess(@NotNull List<String> digits,
+									@NotNull List<String> signs,
+									int iterationIndex) {
+		digits.set(iterationIndex - 1, calcPair(digits, signs.get(iterationIndex), iterationIndex));
+		digits.remove(iterationIndex);
+		signs.remove(iterationIndex);
+	}
+
+	private static boolean isDivisionOrMultiplication(@NotNull String sign) {
+		return sign.equals("*") || sign.equals("/");
+	}
+
+	private static boolean isComplexityOrSubtraction(@NotNull String sign) {
+		return sign.equals("+") || sign.equals("-");
+	}
 
 }
